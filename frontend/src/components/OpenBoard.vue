5e22993bb527c124e556d8e40132df6d6a342e60
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-11-23 18:40:03 +0100

    Port open board to vue component

---
+++ frontend/src/components/OpenBoard.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div id="openboard" style="display: none">
+    <div
+      id="dropfile"
+      onclick="openFile()"
+      ondrop="drop(event)"
+      ondragover="allowDrop(event); this.style.background = 'rgba(255,255,255,.5)'"
+    >
+      <span style="font-size: 20px">Choose a .board file</span>
+      <br />
+      Or drop a file in this box
+    </div>
+
+    <script>
+      function allowDrop(e) {
+        e.preventDefault();
+      }
+
+      const drop = (e) => {
+        e.preventDefault();
+        readFile(e.dataTransfer);
+      };
+    </script>
+
+    <br />
+    <span style="text-decoration: underline">Connect to a BOOLR server</span>
+  </div>
+</template>

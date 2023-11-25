42b7a4fa9d8da32cd892edca2b7d943813d3e4d1
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-11-25 09:57:37 +0100

    Refactor Waypoints component

--- frontend/src/components/WaypointsMenu.vue
+++ frontend/src/components/WaypointsMenu.vue
@@ -1,75 +1,112 @@
 <script lang="ts" setup>
-// let waypoints = [];
+import { ref } from "vue";
+import { WayPoint } from "../types";
 
-// function setWaypoint(x,y,label = `Waypoint#${waypoints.length}`) {
-//     waypoints.push({
-//         x,y,
-//         label
-//     });
-//     toolbar.message(`Waypoint <i>${label}</i> set at <i>${x}</i>,<i>${y}</i>`);
-// }
+const wayPointText = ref("");
+const waypointsMenuIsVisible = ref(false);
+let waypoints: WayPoint[] = [];
 
-// function gotoWaypoint(index) {
-//     if(!waypoints[index]) return;
-//     scroll(waypoints[index].x - mouse.grid.x, waypoints[index].y - mouse.grid.y);
-//     toolbar.message(`Jumped to waypoint#${index} at ${waypoints[index].x}, ${waypoints[index].y}`);
-// }
+function setWaypoint(
+  x: number,
+  y: number,
+  label = `Waypoint#${waypoints.length}`
+) {
+  waypoints.push({
+    x,
+    y,
+    label,
+  });
+  toolbar.message(`Waypoint <i>${label}</i> set at <i>${x}</i>,<i>${y}</i>`);
+}
 
-// const waypointsMenu = document.getElementById("waypointsMenu");
+function gotoWaypoint(index: number) {
+  if (!waypoints[index]) return;
+  scroll(waypoints[index].x - mouse.grid.x, waypoints[index].y - mouse.grid.y);
+  toolbar.message(
+    `Jumped to waypoint#${index} at ${waypoints[index].x}, ${waypoints[index].y}`
+  );
+}
 
-// waypointsMenu.show = function(
-//     x = mouse.screen.x / zoom + offset.x,
-//     y = -mouse.screen.y / zoom + offset.y
-// ) {
-//     if(waypoints.length == 0) {
-//         toolbar.message("You have no waypoints set. Press <i>s</i> to add a waypoint")
-//         return;
-//     }
-//     else if(waypoints.length == 1 && contextMenu.style.display == "none") { gotoWaypoint(0); return }
+show = function (
+  x = mouse.screen.x / zoom + offset.x,
+  y = -mouse.screen.y / zoom + offset.y
+) {
+  if (waypoints.length == 0) {
+    toolbar.message(
+      "You have no waypoints set. Press <i>s</i> to add a waypoint"
+    );
+    return;
+  } else if (waypoints.length == 1 && contextMenu.style.display == "none") {
+    gotoWaypoint(0);
+    return;
+  }
 
-//     this.x = x;
-//     this.y = y;
+  this.x = x;
+  this.y = y;
 
-//     this.innerHTML = "Jump to...";
-//     for(let i = 0; i < waypoints.length; ++i) {
-//         const li = document.createElement("li");
-//         li.onclick = () => {
-//             gotoWaypoint(i);
-//             this.hide();
-//             contextMenu.hide();
-//             c.focus();
-//         }
+  wayPointText.value = "Jump to...";
+  for (let i = 0; i < waypoints.length; ++i) {
+    const li = document.createElement("li");
+    li.onclick = () => {
+      gotoWaypoint(i);
+      this.hide();
+      contextMenu.hide();
+      c.focus();
+    };
 
-//         li.innerHTML = waypoints[i].label;
-//         li.innerHTML += `\t|\t${waypoints[i].x},${waypoints[i].y}`;
+    li.innerHTML = waypoints[i].label;
+    li.innerHTML += `\t|\t${waypoints[i].x},${waypoints[i].y}`;
 
-//         const deleteBtn = document.createElement("i");
-//         deleteBtn.className = "material-icons remove";
-//         deleteBtn.innerHTML = "close";
-//         deleteBtn.onclick = e => {
-//             e.stopPropagation();
-//             if(waypoints.length <= 1) { this.style.display = "none"; this.style.opacity = 0; c.focus(); }
+    const deleteBtn = document.createElement("i");
+    deleteBtn.className = "material-icons remove";
+    deleteBtn.innerHTML = "close";
+    deleteBtn.onclick = (e) => {
+      e.stopPropagation();
+      if (waypoints.length <= 1) {
+        this.style.display = "none";
+        this.style.opacity = 0;
+        c.focus();
+      }
 
-//             const index = Array.from(this.children).indexOf(deleteBtn.parentNode) - 1;
-//             toolbar.message(`Removed waypoint ${waypoints[index].label}`)
-//             waypointsMenu.removeChild(deleteBtn.parentNode);
-//             waypoints.splice(index,1);
-//         }
-//         li.appendChild(deleteBtn);
+      const index = Array.from(this.children).indexOf(deleteBtn.parentNode) - 1;
+      toolbar.message(`Removed waypoint ${waypoints[index].label}`);
+      waypointsMenu.removeChild(deleteBtn.parentNode);
+      waypoints.splice(index, 1);
+    };
+    li.appendChild(deleteBtn);
 
-//         this.appendChild(li);
-//     }
+    this.appendChild(li);
+  }
 
-//     this.style.display = "block";
-//     setTimeout(() => this.style.opacity = .95, 1);
-// }
+  this.style.display = "block";
+  setTimeout(() => (this.style.opacity = 0.95), 1);
+};
 
-// waypointsMenu.hide = function() {
-//     this.style.opacity = 0;
-//     setTimeout(() => this.style.display = "none", 200);
-// }
+const hide = function () {
+  waypointsMenuIsVisible.value = false;
+};
 </script>
 
 <template>
-  <ul id="waypointsMenu"></ul>
+  <ul
+    id="waypointsMenu"
+    :class="{ '#waypointsMenu--hide': !waypointsMenuIsVisible }"
+  >
+    {{
+      wayPointText
+    }}
+  </ul>
 </template>
+
+<style scoped>
+#waypointsMenu--show {
+  display: block;
+  opacity: 0.95;
+  transition: display 200ms ease;
+}
+
+#waypointsMenu--hide {
+  display: none;
+  opacity: 0;
+}
+</style>
